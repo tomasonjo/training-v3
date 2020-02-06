fc646aafde5e3581c3d5b96fd68a0d270c7df6fc
Guillaume Grossetie <ggrossetie@gmail.com>
2020-02-06 20:08:25 +0100

    Aggregate all the speaker notes on a given section into a single open block

---
+++ resources/extensions/revealjs_speaker_notes_aggregator_tree_processor.rb
@@ -0,0 +1,25 @@
+require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
+
+include Asciidoctor
+
+class RevealJsSpeakerNotesAggregatorTreeProcessor < Extensions::TreeProcessor; use_dsl
+  def process(document)
+    if document.backend == 'revealjs'
+      document.find_by({context: :section}).each do |section|
+        notes_blocks = section.blocks.select { |block| block.context == :open && block.roles.include?('notes') }
+        next if notes_blocks.empty?
+        agg_notes_block = Block.new(section, :open, {source: source, attributes: {role: 'notes'}})
+        notes_blocks.each do |notes_block|
+          section.blocks.delete(notes_block)
+          agg_notes_block << notes_block
+        end
+        section.blocks << agg_notes_block
+      end
+    end
+    document
+  end
+end
+
+Extensions.register do
+  tree_processor RevealJsSpeakerNotesAggregatorTreeProcessor
+end
