4474712da433afaf783f2763609f31d9e1649c35
Guillaume Grossetie <ggrossetie@gmail.com>
2020-02-19 11:36:01 +0100

    Cleanup

--- buildSrc/src/main/kotlin/com/neo4j/gradle/asciidoctor/AsciidoctorTableOfContentsPlugin.kt
+++ buildSrc/src/main/kotlin/com/neo4j/gradle/asciidoctor/AsciidoctorTableOfContentsPlugin.kt
@@ -5,18 +5,13 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.ConfigurableFileTree
-import org.gradle.api.internal.file.ImmutablePatternSet
-import org.gradle.api.model.ObjectFactory
-import org.gradle.api.provider.Property
-import org.gradle.api.tasks.*
-import org.gradle.api.tasks.util.PatternFilterable
-import org.gradle.api.tasks.util.PatternSet
-import org.gradle.kotlin.dsl.property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.TaskAction
 import java.io.File
 
 
-open class AsciidoctorTableOfContentsExtension(objects: ObjectFactory) {
-}
+open class AsciidoctorTableOfContentsExtension
 
 open class AsciidoctorTableOfContentsPlugin : Plugin<Project> {
 
@@ -32,22 +27,18 @@ abstract class AsciidoctorTableOfContentsGenerateTask : DefaultTask() {
   @Input
   var outputDir: String = ""
 
-  @Internal
-  var patternSet: PatternFilterable = ImmutablePatternSet.of(PatternSet())
-
   @TaskAction
   fun task() {
-    logger.info(patternSet.toString())
     val asciidoctor = Asciidoctor.Factory.create()
     val data = sources.map { it.sorted() }.flatten().sorted().map { file ->
       val document = asciidoctor.loadFile(file, emptyMap())
       "${file.name.removeSuffix(".adoc")}.html" to document.doctitle
     }.toMap()
     val content = """ASCIIDOCTOR_TABLE_OF_CONTENTS = {
-${data.map { "\"${it.key}\" => \"${it.value}\""}.joinToString(", ") }
+${data.map { "\"${it.key}\" => \"${it.value}\"" }.joinToString(", ")}
 }
 """
-    File(outputDir,"asciidoctor_table_of_contents.rb").writeText(content)
+    File(outputDir, "asciidoctor_table_of_contents.rb").writeText(content)
   }
 
   fun setSource(source: String) {
