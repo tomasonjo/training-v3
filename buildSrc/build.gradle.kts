cdcfe6bbbea795abf0110ea8b3fe64e9f4c7eaca
Guillaume Grossetie <ggrossetie@gmail.com>
2020-04-14 15:24:15 +0200

    Allow to enable plugins selectively with -Pplugins-enabled

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -21,20 +21,30 @@ dependencies {
 
 version = "0.0.1"
 
+val pluginsEnabled = project.findProperty("plugins-enabled") as String?
+
+fun isPluginEnabled(name: String): Boolean = pluginsEnabled == null || pluginsEnabled.isEmpty() || (pluginsEnabled.contains(name))
+
 gradlePlugin {
   plugins {
-    create("s3Plugin") {
-      id = "com.neo4j.gradle.s3.S3Plugin"
-      implementationClass = "com.neo4j.gradle.s3.S3Plugin"
+    if (isPluginEnabled("s3")) {
+      create("s3Plugin") {
+        id = "com.neo4j.gradle.s3.S3Plugin"
+        implementationClass = "com.neo4j.gradle.s3.S3Plugin"
+      }
     }
-    create("asciidoctorModuleDescriptorPlugin") {
-      id = "com.neo4j.gradle.asciidoctor.AsciidoctorModuleDescriptorPlugin"
-      implementationClass = "com.neo4j.gradle.asciidoctor.AsciidoctorModuleDescriptorPlugin"
+    if (isPluginEnabled("asciidoctorModuleDescriptor")) {
+      create("asciidoctorModuleDescriptorPlugin") {
+        id = "com.neo4j.gradle.asciidoctor.AsciidoctorModuleDescriptorPlugin"
+        implementationClass = "com.neo4j.gradle.asciidoctor.AsciidoctorModuleDescriptorPlugin"
+      }
     }
-    create("wordpressPlugin") {
-      id = "com.neo4j.gradle.wordpress.WordPressPlugin"
-      implementationClass = "com.neo4j.gradle.wordpress.WordPressPlugin"
-      version = "0.0.2"
+    if (isPluginEnabled("wordpress")) {
+      create("wordpressPlugin") {
+        id = "com.neo4j.gradle.wordpress.WordPressPlugin"
+        implementationClass = "com.neo4j.gradle.wordpress.WordPressPlugin"
+        version = "0.0.2"
+      }
     }
   }
 }
@@ -44,27 +54,33 @@ pluginBundle {
   vcsUrl = "https://github.com/neo4j-contrib/training-v3"
 
   (plugins) {
-    "s3Plugin" {
-      id = "com.neo4j.gradle.s3.S3Plugin"
-      displayName = "Publish files to Amazon S3"
-      description = "A plugin to publish files to Amazon S3"
-      version = "0.0.1"
-      tags = listOf("s3", "publish", "files")
+    if (isPluginEnabled("s3")) {
+      "s3Plugin" {
+        id = "com.neo4j.gradle.s3.S3Plugin"
+        displayName = "Publish files to Amazon S3"
+        description = "A plugin to publish files to Amazon S3"
+        version = "0.0.1"
+        tags = listOf("s3", "publish", "files")
+      }
     }
     // should be a private plugin
-    "asciidoctorModuleDescriptorPlugin" {
-      id = "com.neo4j.gradle.asciidoctor.AsciidoctorModuleDescriptorPlugin"
-      displayName = "Generate a very specific module descriptor from an AsciiDoc file"
-      description = "A plugin to generate a very specific module descriptor file in a YAML format from and AsciiDoc file."
-      version = "0.0.1"
-      tags = listOf("neo4j")
+    if (isPluginEnabled("asciidoctorModuleDescriptor")) {
+      "asciidoctorModuleDescriptorPlugin" {
+        id = "com.neo4j.gradle.asciidoctor.AsciidoctorModuleDescriptorPlugin"
+        displayName = "Generate a very specific module descriptor from an AsciiDoc file"
+        description = "A plugin to generate a very specific module descriptor file in a YAML format from and AsciiDoc file."
+        version = "0.0.1"
+        tags = listOf("neo4j")
+      }
     }
-    "wordpressPlugin" {
-      id = "com.neo4j.gradle.wordpress.WordPressPlugin"
-      displayName = "Publish posts and pages to WordPress"
-      description = "A plugin to publish posts or pages to WordPress from an HTML file and a YAML file that contains metadata"
-      version = "0.0.2"
-      tags = listOf("wordpress", "publish", "posts", "pages")
+    if (isPluginEnabled("wordpress")) {
+      "wordpressPlugin" {
+        id = "com.neo4j.gradle.wordpress.WordPressPlugin"
+        displayName = "Publish posts and pages to WordPress"
+        description = "A plugin to publish posts or pages to WordPress from an HTML file and a YAML file that contains metadata"
+        version = "0.0.2"
+        tags = listOf("wordpress", "publish", "posts", "pages")
+      }
     }
   }
 }
